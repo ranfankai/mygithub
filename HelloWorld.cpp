d095ddd9443a82c445a6488c4dd72a3bd448deb6
ranfankai <528758769@qq.com>
2013-09-03 22:48:29 +0800

    HelloWorld
    First

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+名称：HelloWorld
+日期：2013-09-03
+作者：冉凡凯
+学号：2012060080030*/
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+int main()
+{
+	
+	cout<<"HelloWorld"<<endl<<"I am ranfankai"<<endl;
+	return 0;
+}
+
